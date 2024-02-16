80e4dedf55ee9e11bd671ace5ba3764483baf7c9
vijay <kvijay20204567@gmail.com>
2024-02-16 20:23:27 +0530

    setuped-backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const app=express();
+app.port=3000;
+app.use(express.json());
+app.post("/todo",function(req,res){
+
+})
+app.get("/todos",function(req,res){
+
+})
+app.put("/completed",function(req,res){
+
+})
+app.listen(3000);
