208aa50c79fcd840e8fa77f04a132d6a4576be3a
K VIJAY <146370607+k-vijay-05@users.noreply.github.com>
2024-05-20 00:41:31 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://vijay:kvijay%401026354@cluster0.xwkkj0v.mongodb.net/todos")
 const todoschema= mongoose.Schema({
     title: String,
     description:String,
